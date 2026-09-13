afdabea3b040e724b43ce2d9605a646b37ef3491
Денис Берёзкин <Tyuiu.BerezkinDA@outlook.com>
2026-09-13 15:24:16 +0300

    Task3

---
+++ Tyuiu.BerezkinDA.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BerezkinDA.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.BerezkinDA.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
